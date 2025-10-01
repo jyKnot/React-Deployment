a774c8f427934c9cc5d8a7d0c75fe910e0bf5211
Jill Wynott <145140530+jyknot@users.noreply.github.com>
2025-09-30 21:31:07 -0700

    Updated key

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // import.meta.env.VITE_API_URL;
 const API_URL = import.meta.env.VITE_API_URL;
 
-// Get all product
+// Get all products
 export const getProducts = async () => {
   try {
     const response = await axios.get(API_URL);
